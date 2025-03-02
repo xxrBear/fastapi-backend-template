b6cf127770bff22839efcd20f836396caf8fd2ff
bear <xxrBear@gmail.com>
2025-03-02 14:52:22 +0800

    格式化项目风格

--- app/user/api.py
+++ app/user/api.py
@@ -8,14 +8,14 @@ from common.resp import json_data
 from core.utils import adapter_records_info
 from crud import user
 from crud.user import delete_user_by_id
-from models.user import UserCreate, User, UserLogin, UserPage, UserDelete
+from models.user import User, UserCreate, UserDelete, UserLogin, UserPage
 
 router = APIRouter()
 
 
-@router.post('/register')
-async def register(session: SessionDep, user_create: UserCreate):
-    """ 用户注册
+@router.post("/register")
+def register(session: SessionDep, user_create: UserCreate):
+    """用户注册
     :param session:
     :param user_create:
     :return:
@@ -24,54 +24,59 @@ async def register(session: SessionDep, user_create: UserCreate):
     return json_data(data=user_obj.to_dict())
 
 
-@router.post('/login')
-async def login(request: Request, session: SessionDep, user_in: UserLogin):
+@router.post("/login")
+def login(request: Request, session: SessionDep, user_in: UserLogin):
     user_obj = user.validate_login_info(session, user_in)
-    request.session['user_login_state'] = user_obj.to_dict()
+    request.session["user_login_state"] = user_obj.to_dict()
     return json_data(message="登录成功")
 
 
-@router.post('/logout')
-async def logout(request: Request):
-    request.session['user_login_state'] = {}
-    return json_data(message='退出成功')
+@router.post("/logout")
+def logout(request: Request):
+    request.session["user_login_state"] = {}
+    return json_data(message="退出成功")
 
 
-@router.get('/get/login')
-async def get_active_user_login(request: Request):
-    """ 获取当前登录用户
+@router.get("/get/login")
+def get_active_user_login(request: Request):
+    """获取当前登录用户
     :param request:
     :return:
     """
-    user_login_state = request.session.get('user_login_state')
+    user_login_state = request.session.get("user_login_state")
     if not user_login_state:
         return json_data(**{"message": "未登录", "code": state.USER_NOT_LOGIN})
     else:
         return json_data(data=user_login_state)
 
 
-@router.post('/list/page')
-async def get_all_users(session: SessionDep, user_in: UserPage):
-    """ 获取当前所有未删除的用户
+@router.post("/list/page")
+def get_all_users(session: SessionDep, user_in: UserPage):
+    """获取当前所有未删除的用户
     :param session:
     :param user_in:
     :return:
     """
-    sql = select(User).where(User.is_delete == False).offset(user_in.current - 1).limit(user_in.pageSize)
+    sql = (
+        select(User)
+        .where(User.is_delete)
+        .offset(user_in.current - 1)
+        .limit(user_in.pageSize)
+    )
 
     if user_in.userName:
-        sql = sql.where(User.user_name.like('%{}%'.format(user_in.userName)))
+        sql = sql.where(User.user_name.like(f"%{user_in.userName}%"))
     if user_in.userProfile:
-        sql = sql.where(User.user_profile.like('%{}%'.format(user_in.userProfile)))
+        sql = sql.where(User.user_profile.like(f"%{user_in.userProfile}%"))
     user_objs = session.exec(sql).all()
     data = adapter_records_info(user_objs, user_in.pageSize)
-    data.update({'records': [user.to_dict() for user in user_objs]})
+    data.update({"records": [user.to_dict() for user in user_objs]})
     return json_data(data=data)
 
 
-@router.post('/delete')
-async def delete_all_users(session: SessionDep, user_del: UserDelete, request: Request):
-    """ 删除用户
+@router.post("/delete")
+def delete_all_users(session: SessionDep, user_del: UserDelete, request: Request):
+    """删除用户
     :param session:
     :param user_del:
     :param request:
--- common/execptions.py
+++ common/execptions.py
@@ -3,36 +3,34 @@ from common.resp import json_data
 
 
 class ValidateError(Exception):
-    """ 自定义参数异常类
-    """
+    """自定义参数异常类"""
 
     def __init__(self, name: str):
         self.name = name
 
 
 class RuntimeErr(Exception):
-    """ 自定义运行异常类
-    """
+    """自定义运行异常类"""
 
     def __init__(self, name: str):
         self.name = name
 
 
-async def validate_exception_handler(exc: ValidateError):
+def validate_exception_handler(exc: ValidateError):
     """
     统一处理 ValidateError 及其子类异常的方法
     """
     return json_data(code=state.REQUEST_PARAMS_ERROR, message=exc.name)
 
 
-async def runtime_exception_handler(exc: RuntimeErr):
+def runtime_exception_handler(exc: RuntimeErr):
     """
     统一处理 RuntimeErr 及其子类异常的方法
     """
     return json_data(code=state.OPERATION_ERROR, message=exc.name)
 
 
-def validate_request_exception(statement, message=''):
+def validate_request_exception(statement, message=""):
     """
     当 statement 为 True 时，抛出请求参数异常
     :param statement:
@@ -41,6 +39,3 @@ def validate_request_exception(statement, message=''):
     """
     if statement:
         raise ValidateError(message)
-
-
-
