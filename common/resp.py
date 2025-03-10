cc90912b18535193e3784fe77d336b55962bcf60
bear <xxrBear@gmail.com>
2025-03-10 21:18:37 +0800

    修改返回结果

--- common/resp.py
+++ common/resp.py
@@ -1,12 +1,11 @@
 from fastapi.responses import JSONResponse
 
 
-def json_data(code=0, message="ok", description=None, data=None):
+def json_data(code=200, message="ok", description=None, data=None):
     """统一返回 json 格式
     :return:
     """
     return JSONResponse(
-        status_code=200,
         content={
             "code": code,
             "message": message,
